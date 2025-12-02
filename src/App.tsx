f3aa88f97c4cc66994ca3fb61dcd48f1d585369c
Digitalqatalyst <dq.demo.a@digitalqatalyst.com>
2025-12-02 20:29:07 +0300

    build fixes

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,6 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
 import { Toaster } from "./components/ui/toaster";
-import { Toaster as Sonner } from "./components/ui/sonner";
 import { TooltipProvider } from "./components/ui/tooltip";
 import { ThemeProvider } from "./components/ThemeProvider";
 import { PortfolioProvider } from "./context/PortfolioContext";
@@ -17,7 +16,6 @@ const App = () => (
       <PortfolioProvider>
         <TooltipProvider>
           <Toaster />
-          <Sonner />
           <BrowserRouter>
             <Routes>
               <Route path="/" element={<Index />} />
